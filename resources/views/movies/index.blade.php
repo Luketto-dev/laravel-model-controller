d4a71f17f090c05e6d41697afe4717f25301eb50
Luketto-dev <luca.francesconi1995@gmail.com>
2022-07-28 18:00:38 +0200

    creazione card-movie nella vista movies.index

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,11 +3,21 @@
 
 @section('main_content')
 <div class="container">
-    <div class="movies-list">
-        <div class="row row-cols-5">
+    <h1>Lista film</h1>
+    <div class="movies-list pt-5">
+        <div class="row row-cols-5 gy-3">
             @foreach ($movies as $movie)
             <div class="col">
-                
+                <div class="movie-card">
+                    <div class="overlay"></div>
+                    <div class="card-content">
+                        <h3>{{$movie['title']}}</h3>
+                        <h6>Titolo originale: {{$movie['original_title']}}</h6>
+                        <h6>Nazionalità: {{$movie['nazionality']}}</h6>
+                        <h6>Data Produzione: {{date('F, Y', strtotime($movie['date']))}}</h6>
+                        <h6>voto: {{$movie['vote']}}</h6>
+                    </div>
+                </div>
             </div>
             @endforeach
         </div>
